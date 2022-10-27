669ea29fffdeefad23b00aacd23080f10eef0378
Enzo Piriou <enzopiriou@gmail.com>
2022-10-27 16:36:00 +0200

    add debug PRINTF in utils.c functions

--- src/utils.c
+++ src/utils.c
@@ -11,6 +11,8 @@
 ** return 0 Ok
 */
 uint8_t add_uint256(uint8_t *a, uint8_t *b) {
+    PRINTF("ADD256 before\ta:\t%.*H\n", INT256_LENGTH, a);
+    PRINTF("ADD256 before\tb:\t%.*H\n", INT256_LENGTH, b);
     uint8_t carry = 0;
     for (uint8_t i = INT256_LENGTH - 1; i > 0; i--) {
         uint16_t added = a[i] + b[i] + carry;
@@ -20,6 +22,8 @@ uint8_t add_uint256(uint8_t *a, uint8_t *b) {
     uint16_t added = a[0] + b[0] + carry;
     if (added > 255) return 1;
     a[0] += b[0] + carry;
+    PRINTF("ADD256 after\ta:\t%.*H\n", INT256_LENGTH, a);
+    PRINTF("ADD256 after\tb:\t%.*H\n", INT256_LENGTH, b);
     return 0;
 }
 
@@ -61,11 +65,17 @@ uint8_t calc_number_of_nfts(uint8_t *amount,
                             uint32_t numerator,
                             uint32_t denominator,
                             uint16_t *number_of_nfts) {
-    PRINTF("calc_number_of_nfts(): sizeof 4 == %d\n", sizeof(uint32_t));
+    // TODO number_of_nfts should be same size as value, check all sizes
+    PRINTF("NB_NFT numerator: %d\n", numerator);
+    PRINTF("NB_NFT denominator: %d\n", denominator);
+    PRINTF("NB_NFT amount: %.*H\n", INT256_LENGTH, amount);
+    PRINTF("NB_NFT number_of_nfts: %d\n", *number_of_nfts);
+    PRINTF("NB_NFT --------\n");
+
     uint32_t value = 0;
-    PRINTF("calc_number_of_nfts(): sizeof value == %d\n", sizeof(value));
     if (does_number_fit(amount, PARAMETER_LENGTH, sizeof(value))) return 1;
-    value = U2BE(amount, INT256_LENGTH - sizeof(value));
+    value = U4BE(amount, INT256_LENGTH - sizeof(value));
+    PRINTF("NB_NFT value: %d\n", value);
     if (numerator == denominator) {
         (*number_of_nfts) += value;
         if ((*number_of_nfts) < value) return 1;  // overflow
@@ -81,7 +91,10 @@ uint8_t calc_number_of_nfts(uint8_t *amount,
     // if (!exact) revert
     if (!(((newValue * denominator) / numerator) == value)) return 2;
     // Add this order's number_of_nfts to global number_of_nfts.
+    PRINTF("NB_NFT newValue: %d\n", newValue);
+    PRINTF("NB_NFT number_of_nfts before: %d\n", *number_of_nfts);
     (*number_of_nfts) += newValue;
+    PRINTF("NB_NFT number_of_nfts after: %d\n", *number_of_nfts);
     if ((*number_of_nfts) < newValue) return 1;  // overflow
     return 0;
 }
@@ -91,7 +104,7 @@ uint8_t calc_number_of_nfts(uint8_t *amount,
 */
 
 void swap_tokens(context_t *context) {
-    PRINTF("Swaping tokens.\n");
+    PRINTF("\n\nSwaping tokens.\n\n");
     token_t save;
     save = context->token1;
     context->token1 = context->token2;
